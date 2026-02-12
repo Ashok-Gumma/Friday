fad804aa1220fd78b79dbdab8b5f37f6ae2c744f
Ashok Gumma <ashokgumma20@gmail.com>
2026-02-12 22:21:23 +0530

    c

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// ✅ Correct relative paths
-import "/styles/index.css";
-import "/styles/main.css";
+import "../styles/index.css";
+import "../styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
